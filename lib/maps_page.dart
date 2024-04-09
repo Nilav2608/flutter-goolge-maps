b248dcd7792279fd50e6f7542de01bd4a3c1e4a2
Nilavarasu2608 <knlsu444@gmail.com>
2024-04-09 23:50:22 +0530

    fetch directions info

--- lib/maps_page.dart
+++ lib/maps_page.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:maps_app/constants.dart';
 import 'package:maps_app/repo/map_repo.dart';
+import 'package:maps_app/widgets/bottom_card.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleMapsPage extends StatefulWidget {
   const GoogleMapsPage({super.key});
@@ -18,6 +21,9 @@ const googlePlex = LatLng(37.422131, -122.084801);
 const applePark = LatLng(37.3346, -122.0090);
 
 LatLng? currentPosition;
+
+String? currentDistance;
+String? currentDuration;
 
 final Completer<GoogleMapController> mapController =
     Completer<GoogleMapController>();
@@ -68,8 +74,6 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
         .animateCamera(CameraUpdate.newCameraPosition(mapCameraPosition));
   }
 
-  
-
   void generatePolylines(List<LatLng> polylineCoordinates) async {
     PolylineId id = const PolylineId("poly");
     Polyline polyline = Polyline(
@@ -84,17 +88,40 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
 
   Future initializeMap() async {
     await fetchLocationUpdates();
-    final coordinates = await MapRepository().getPolyLinePoints(apiKey: apiKey,sourceLocation: googlePlex,destLocation: applePark);
+    final coordinates = await MapRepository().getPolyLinePoints(
+        apiKey: apiKey, sourceLocation: googlePlex, destLocation: applePark);
     generatePolylines(coordinates);
+    await fetchDistanceAndDuration();
+  }
+
+  Future<void> fetchDistanceAndDuration() async {
+    final String origin =
+        '${currentPosition!.latitude},${currentPosition!.longitude}';
+    final String destination = '${applePark.latitude},${applePark.longitude}';
+    final String url =
+        'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&key=$apiKey';
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final decoded = json.decode(response.body);
+      setState(() {
+        currentDistance =
+            decoded['routes'][0]['legs'][0]['distance']['text'] as String;
+        currentDuration =
+            decoded['routes'][0]['legs'][0]['duration']['text'] as String;
+      });
+    } else {
+      throw Exception('Failed to fetch distance and duration');
+    }
   }
 
   @override
   void initState() {
     super.initState();
     // fetching the location after rendering the UI
-      WidgetsBinding.instance.addPostFrameCallback((_) async {
-        await fetchLocationUpdates().then((_) => initializeMap());
-      });
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await fetchLocationUpdates().then((_) => initializeMap());
+    });
   }
 
   @override
@@ -102,28 +129,38 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
     return Scaffold(
       body: currentPosition == null
           ? const Center(child: CircularProgressIndicator())
-          : GoogleMap(
-              onMapCreated: (GoogleMapController controller) =>
-                  mapController.complete(controller),
-              initialCameraPosition:
-                  const CameraPosition(target: googlePlex, zoom: 13),
-              markers: {
-                Marker(
-                  markerId: const MarkerId('currentLocation'),
-                  icon: BitmapDescriptor.defaultMarker,
-                  position: currentPosition!,
-                ),
-                const Marker(
-                    markerId: MarkerId('sourceLoaction'),
+          : Stack(children: [
+              GoogleMap(
+                onMapCreated: (GoogleMapController controller) =>
+                    mapController.complete(controller),
+                initialCameraPosition:
+                    const CameraPosition(target: googlePlex, zoom: 13),
+                markers: {
+                  Marker(
+                    markerId: const MarkerId('currentLocation'),
                     icon: BitmapDescriptor.defaultMarker,
-                    position: googlePlex),
-                const Marker(
-                    markerId: MarkerId('destinationLoaction'),
-                    icon: BitmapDescriptor.defaultMarker,
-                    position: applePark)
-              },
-              polylines: Set<Polyline>.of(polyLines.values),
-            ),
+                    position: currentPosition!,
+                  ),
+                  const Marker(
+                      markerId: MarkerId('sourceLoaction'),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: googlePlex),
+                  const Marker(
+                      markerId: MarkerId('destinationLoaction'),
+                      icon: BitmapDescriptor.defaultMarker,
+                      position: applePark)
+                },
+                polylines: Set<Polyline>.of(polyLines.values),
+              ),
+               Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 10,
+                  child: BottomCard(
+                    distance: currentDistance??'',
+                    duration: currentDuration??'',
+                  ))
+            ]),
     );
   }
 }
