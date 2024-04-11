3c1bf5b2227f9ab7b868b011f9e6de1d3a0615f6
Nilavarasu2608 <knlsu444@gmail.com>
2024-04-11 23:27:17 +0530

    Refactor

--- lib/maps_page.dart
+++ lib/maps_page.dart
@@ -1,10 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 import 'package:maps_app/constants.dart';
+import 'package:maps_app/models/directions.model.dart';
+import 'package:maps_app/repo/directions_repository.dart';
 import 'package:maps_app/repo/map_repo.dart';
 import 'package:maps_app/widgets/bottom_card.dart';
 import 'package:http/http.dart' as http;
@@ -28,49 +31,21 @@ String? currentDuration;
 final Completer<GoogleMapController> mapController =
     Completer<GoogleMapController>();
 
+Marker? originMarker;
+Marker? destinationMarker;
+
 Map<PolylineId, Polyline> polyLines = {};
 
 class _GoogleMapsPageState extends State<GoogleMapsPage> {
-  //ask permission to access the current location
-  Future<void> fetchLocationUpdates() async {
-    bool serviceEnabled;
-    PermissionStatus status;
-    // check the location service of the device is enabled
-    serviceEnabled = await locationController.serviceEnabled();
-
-    if (serviceEnabled) {
-      serviceEnabled = await locationController.requestService();
-    } else {
-      return;
-    }
-
-    status = await locationController.hasPermission();
-    if (status == PermissionStatus.denied) {
-      status = await locationController.requestPermission();
-      if (status != PermissionStatus.granted) {
-        return;
-      }
-    }
-    locationController.onLocationChanged.listen((currentLocation) {
-      if (currentLocation.latitude != null &&
-          currentLocation.longitude != null) {
-        setState(() {
-          currentPosition =
-              LatLng(currentLocation.latitude!, currentLocation.longitude!);
-          cameraPosition(currentPosition!);
-        });
-      }
-    });
-  }
+  // ask permission to access the current location
 
   Future<void> cameraPosition(LatLng markerPosition) async {
     // create an variable to access the mapController from the onMapCreated: property
-    final GoogleMapController controller = await mapController.future;
     //current map camera position
     CameraPosition mapCameraPosition =
-        CameraPosition(target: markerPosition, zoom: 13);
+        CameraPosition(target: markerPosition, zoom: 15);
     //animate the camera to the current location
-    await controller
+    await mapController!
         .animateCamera(CameraUpdate.newCameraPosition(mapCameraPosition));
   }
 
@@ -87,80 +62,141 @@ class _GoogleMapsPageState extends State<GoogleMapsPage> {
   }
 
   Future initializeMap() async {
-    await fetchLocationUpdates();
+    // await fetchLocationUpdates();
     final coordinates = await MapRepository().getPolyLinePoints(
         apiKey: apiKey, sourceLocation: googlePlex, destLocation: applePark);
     generatePolylines(coordinates);
-    await fetchDistanceAndDuration();
   }
 
-  Future<void> fetchDistanceAndDuration() async {
-    final String origin =
-        '${currentPosition!.latitude},${currentPosition!.longitude}';
-    final String destination = '${applePark.latitude},${applePark.longitude}';
-    final String url =
-        'https://maps.googleapis.com/maps/api/directions/json?origin=$origin&destination=$destination&key=$apiKey';
+  static const initialCameraPosition =
+      CameraPosition(target: googlePlex, zoom: 13.5);
 
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      final decoded = json.decode(response.body);
-      setState(() {
-        currentDistance =
-            decoded['routes'][0]['legs'][0]['distance']['text'] as String;
-        currentDuration =
-            decoded['routes'][0]['legs'][0]['duration']['text'] as String;
-      });
-    } else {
-      throw Exception('Failed to fetch distance and duration');
-    }
+  GoogleMapController? mapController;
+
+  Directions? info;
+
+  @override
+  void dispose() {
+    super.dispose();
+    mapController!.dispose();
   }
 
   @override
   void initState() {
     super.initState();
-    // fetching the location after rendering the UI
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      await fetchLocationUpdates().then((_) => initializeMap());
-    });
+  }
+
+  Dio dioClient = Dio();
+  void addMarker(LatLng pos) async {
+    if (originMarker == null ||
+        (originMarker != null && destinationMarker != null)) {
+      //origin marker is not set OR origin and destination marker both are set
+      setState(() {
+        originMarker = Marker(
+            markerId: const MarkerId('origin'),
+            infoWindow: const InfoWindow(title: 'Origin'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueGreen),
+            position: pos);
+        destinationMarker = null;
+        info = null;
+      });
+    } else {
+      //Origin is already set
+      //Set destination
+      setState(() {
+        destinationMarker = Marker(
+            markerId: const MarkerId('destination'),
+            infoWindow: const InfoWindow(title: 'Destination'),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+            position: pos);
+      });
+      final directions = await DirectionsRepository(dioClient)
+          .getDirections(origin: originMarker!.position, destination: pos);
+
+      setState(() {
+        info = directions;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: currentPosition == null
-          ? const Center(child: CircularProgressIndicator())
-          : Stack(children: [
-              GoogleMap(
-                onMapCreated: (GoogleMapController controller) =>
-                    mapController.complete(controller),
-                initialCameraPosition:
-                    const CameraPosition(target: googlePlex, zoom: 13),
-                markers: {
-                  Marker(
-                    markerId: const MarkerId('currentLocation'),
-                    icon: BitmapDescriptor.defaultMarker,
-                    position: currentPosition!,
-                  ),
-                  const Marker(
-                      markerId: MarkerId('sourceLoaction'),
-                      icon: BitmapDescriptor.defaultMarker,
-                      position: googlePlex),
-                  const Marker(
-                      markerId: MarkerId('destinationLoaction'),
-                      icon: BitmapDescriptor.defaultMarker,
-                      position: applePark)
+      appBar: AppBar(
+        title: const Text("Map"),
+        actions: [
+          if (originMarker != null)
+            TextButton(
+                onPressed: () {
+                  cameraPosition(originMarker!.position);
                 },
-                polylines: Set<Polyline>.of(polyLines.values),
-              ),
-               Positioned(
-                  left: 0,
-                  right: 0,
-                  bottom: 10,
-                  child: BottomCard(
-                    distance: currentDistance??'',
-                    duration: currentDuration??'',
-                  ))
-            ]),
+                child: Text(
+                  "ORIGIN",
+                  style: TextStyle(fontSize: 16, color: Colors.green.shade700),
+                )),
+          if (destinationMarker != null)
+            TextButton(
+                onPressed: () {
+                  cameraPosition(destinationMarker!.position);
+                },
+                child: const Text(
+                  "DEST",
+                  style: TextStyle(
+                      fontSize: 16, color: Color.fromARGB(255, 33, 79, 243)),
+                ))
+        ],
+      ),
+      body:
+          // currentPosition == null
+          //     ? const Center(child: CircularProgressIndicator())
+          //     :
+          Stack(children: [
+        GoogleMap(
+          onLongPress: (LatLng position) => addMarker(position),
+          myLocationButtonEnabled: false,
+          zoomControlsEnabled: false,
+          onMapCreated: (GoogleMapController controller) =>
+              mapController = controller,
+          initialCameraPosition: initialCameraPosition,
+          markers: {
+            const Marker(
+                markerId: MarkerId('sourceLoaction'),
+                icon: BitmapDescriptor.defaultMarker,
+                position: googlePlex),
+            if (originMarker != null) originMarker!,
+            if (destinationMarker != null) destinationMarker!,
+          },
+          polylines: {
+            if (info != null)
+              Polyline(
+                  polylineId: const PolylineId('overView-polyLine'),
+                  color: Colors.blue.shade400,
+                  points: info!.polyinePoints!
+                      .map((e) => LatLng(e.latitude, e.longitude))
+                      .toList(),
+                  width: 6)
+          },
+        ),
+        if (info != null)
+          Positioned(
+              top: 10,
+              child: BottomCard(
+                distance: info!.totalDistance ?? '',
+                duration: info!.totalDuration ?? '',
+              ))
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          mapController!.animateCamera(
+              // info != null
+              // ? CameraUpdate.newLatLngBounds(info.bounds!, 100)
+              // :
+              CameraUpdate.newCameraPosition(initialCameraPosition));
+        },
+        child: const Icon(Icons.center_focus_strong),
+      ),
     );
   }
 }
