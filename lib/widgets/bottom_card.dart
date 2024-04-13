da89192980b4305b63f7cf4c046b45e7c08df646
Nilavarasu2608 <knlsu444@gmail.com>
2024-04-13 23:45:25 +0530

    Bottom Card to info card - displayed on top of the maps screen

--- lib/widgets/bottom_card.dart
+++ lib/widgets/bottom_card.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
-class BottomCard extends StatelessWidget {
+class InfoCard extends StatelessWidget {
   final String distance;
   final String duration;
-  const BottomCard({super.key, required this.distance, required this.duration});
+  final bool isValidLocation;
+  const InfoCard({super.key, required this.distance, required this.duration, required this.isValidLocation});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-        width:  MediaQuery.of(context).size.width,
+        width: MediaQuery.of(context).size.width,
         height: 80,
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(50),
-            color: Colors.white,
-            
+            color: isValidLocation? Colors.green.shade400 : Colors.red.shade400,
             boxShadow: const [
               BoxShadow(
                   offset: Offset(1.0, 0.0),
@@ -25,23 +25,35 @@ class BottomCard extends StatelessWidget {
             ]),
         child: Padding(
           padding: const EdgeInsets.all(20.0),
-          child: Row(
+          child: isValidLocation ? Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Row(
                 children: [
                   Text(
                     "$distance ($duration)",
-                    style: TextStyle(
-                      color: Colors.grey.shade600,
-                      fontSize: 24),
-
+                    style: const TextStyle(color: Colors.white, fontSize: 24),
                   ),
                 ],
               ),
-              Image.asset("assets/car.png",height: 50,width: 50,)
+              Image.asset(
+                "assets/car.png",
+                height: 50,
+                width: 50,
+              )
             ],
-          ),
+          ): const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.warning,color: Colors.white,),
+                Text(
+                          "The selected location is not available",
+                          style: TextStyle(color: Colors.white, fontSize: 14),
+                        ),
+              ],
+            ),
+          ), 
         ),
       ),
     );
